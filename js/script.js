d7413185aea2144b01c2bd8da78f4ae9a7da797a
Veronika <veroniksayas@gmail.com>
2022-10-06 17:13:08 +0300

    Changed all functions and add tasks for localStorage

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ const emptyList = document.querySelector('#emptyList');
 const tasksList = document.querySelector('.tasks__list');
 const clearTaskBtn = document.querySelector('.clearAll-btn');
 
+let tasks = [];
+
+if(localStorage.getItem('tasks')){
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach(task => renderTask(task));
+}
+
+checkEmptyList();
+
 form.addEventListener('submit', addTask);
 tasksList.addEventListener('click', deleteTask);
 tasksList.addEventListener('click', doneTask);
@@ -12,19 +21,129 @@ tasksList.addEventListener('click', editTask);
 clearTaskBtn.addEventListener('click', clearAllTask);
 
 
-// Функция для создание таска
+
 function addTask(event){
     event.preventDefault();
 
     //Текст задачи
     let taskValue = taskInput.value;
 
+    const newTask = {
+        id: Date.now(),
+        title: taskValue,
+        done: false
+    }
+
+    tasks.push(newTask);
+    saveToLocalStorage();
+
+    renderTask(newTask);
+
+    // cleaer taskValue and add focus
+    taskInput.value = ``;
+    taskInput.focus();
+    checkEmptyList();
+
+
+}
+
+function deleteTask(event){
+
+    if(event.target.dataset.action !== 'delete') return;
+
+    const parentNode = event.target.closest('li');
+
+    const idTask = +parentNode.id;
+    tasks = tasks.filter(task => task.id !== idTask)
+
+    saveToLocalStorage()
+
+    parentNode.remove();
+    checkEmptyList();
+}
+
+function doneTask(event){
+    if(event.target.dataset.action !== 'done') return; 
+
+    const parentNode = event.target.closest('li');
+    parentNode.classList.toggle('active');
+
+    const idTask = +parentNode.id;
+    const task = tasks.find(task => task.id === idTask);
+    task.done = !task.done;
+
+    saveToLocalStorage()
+
+    checkEmptyList();
+}
+
+function editTask(event){
+    if(event.target.dataset.action === 'edit'){
+        const parentNode = event.target.closest('li');
+        let taskInput = parentNode.querySelector('.tasks__list-text');
+
+        taskInput.removeAttribute('readonly');
+        taskInput.focus();
+        event.target.innerHTML = `<i class="fa-solid fa-floppy-disk"></i>`;
+        event.target.dataset.action = 'save';
+
+
+    } else if (event.target.dataset.action === 'save') {
+        const parentNode = event.target.closest('li');
+        let taskInput = parentNode.querySelector('.tasks__list-text');
+
+        taskInput.setAttribute('readonly', "readonly");
+        event.target.innerHTML = ` <i class="fa-solid fa-pen"></i>`;
+        event.target.dataset.action = 'edit';
+
+        const idTask = +parentNode.id;
+        const task = tasks.find(task => task.id === idTask);
+        task.title = taskInput.value;
+        saveToLocalStorage()
+    }
+}
+
+function clearAllTask(event){
+    if(tasks.length !== 0) {
+        const parentNode = event.target.closest('div');
+        let allTasks = parentNode.querySelectorAll('.tasks__list-item');
+        allTasks.forEach(task => task.remove())
+        tasks = [];
+        saveToLocalStorage()
+        checkEmptyList();
+    }
+}
+
+function checkEmptyList() {
+    if(tasks.length === 0) {
+        const emptyListHTML = `
+        <li class="tasks__list-empty" id="emptyList">
+            <i class="fa-solid fa-chevron-left"></i>
+            <span>Empty Tasks</span>
+            <i class="fa-solid fa-chevron-right"></i>
+        </li>`;
+
+        tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+    }
+
+    if(tasks.length > 0) {
+        const emptyListEl = document.querySelector('#emptyList');
+        emptyListEl ? emptyListEl.remove() : null;
+    }
+}
+
+
+function saveToLocalStorage(){
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask(task){
     // Разметка задачи
     const taskHTML =`
-        <li class="tasks__list-item">
-            <input type="text" class="tasks__list-text" value="${taskValue}" readonly></input>
+        <li id="${task.id}" class="tasks__list-item ${task.done ? 'active' : ''}">
+            <input type="text" class="tasks__list-text" value="${task.title}" readonly></input>
             <div class="tasks__list-buttons">
-                <input type="checkbox" class="tasks__list-input done-btn" data-action="done"></input>
+                <input type="checkbox" class="tasks__list-input done-btn" data-action="done" ${task.done ? 'checked' : ''}></input>
                 <button class="tasks__list-btn edit-btn" data-action="edit">
                     <i class="fa-solid fa-pen"></i>
                 </button>
@@ -36,72 +155,4 @@ function addTask(event){
     `;
 
     tasksList.insertAdjacentHTML('beforeend', taskHTML)
-
-    // cleaer taskValue and add focus
-    taskInput.value = ``;
-    taskInput.focus();
-
-    // Если есть таск, то убираем надпись "empty list"
-    if(tasksList.children.length > 1) {
-        emptyList.classList.add('hidden');
-    }
-
-}
-
-// deleye task
-function deleteTask(event){
-
-    if(event.target.dataset.action !== 'delete') return;
-
-    const parentNode = event.target.closest('li');
-    parentNode.remove();
-
-    // Если есть таск, то убираем надпись "empty list"
-    if(tasksList.children.length === 1) {
-        emptyList.classList.remove('hidden');
-    }
-}
-
-// done task
-function doneTask(event){
-    if(event.target.dataset.action !== 'done') return; 
-
-    const parentNode = event.target.closest('li');
-    parentNode.classList.toggle('active');
-}
-
-// edit task
-function editTask(event){
-    if(event.target.dataset.action === 'edit'){
-        const parentNode = event.target.closest('li');
-        let taskInput = parentNode.querySelector('.tasks__list-text');
-
-        taskInput.removeAttribute('readonly');
-        taskInput.focus();
-        event.target.innerHTML = `<i class="fa-solid fa-floppy-disk"></i>`;
-        event.target.dataset.action = 'save';
-
-    } else if (event.target.dataset.action === 'save') {
-        const parentNode = event.target.closest('li');
-        let taskInput = parentNode.querySelector('.tasks__list-text');
-
-        taskInput.setAttribute('readonly', "readonly");
-        event.target.innerHTML = ` <i class="fa-solid fa-pen"></i>`;
-        event.target.dataset.action = 'edit';
-
-    }
-}
-
-// clear All Tasks
-function clearAllTask(event){
-    if(tasksList.children.length > 1) {
-        let taskARR = [...tasksList.children];
-        taskARR.forEach(task => {
-            if(task.classList.contains('tasks__list-item')) {
-                console.log(task)
-                task.closest('li').remove();
-                emptyList.classList.remove('hidden');
-            }
-        })
-    }
 }
