379435e3e3bdd30b3fca6b63763ff6ca68f1a974
Veronika <veroniksayas@gmail.com>
2022-10-08 14:48:58 +0300

    Add function for filter tasks btns

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 // Нахожу все нужные мне элементы
 const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
-const emptyList = document.querySelector('#emptyList');
+// const emptyList = document.querySelector('#emptyList');
 const tasksList = document.querySelector('.tasks__list');
 const clearTaskBtn = document.querySelector('.clearAll-btn');
 
 let tasks = [];
+renderTaskFromLocalStorage();
 
-if(localStorage.getItem('tasks')){
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-    tasks.forEach(task => renderTask(task));
-}
 
 checkEmptyList();
 
@@ -31,7 +28,8 @@ function addTask(event){
     const newTask = {
         id: Date.now(),
         title: taskValue,
-        done: false
+        done: false,
+        status: 'pending'
     }
 
     tasks.push(newTask);
@@ -71,7 +69,7 @@ function doneTask(event){
     const idTask = +parentNode.id;
     const task = tasks.find(task => task.id === idTask);
     task.done = !task.done;
-
+    task.status = task.status === "done" ? 'pending' : 'done';
     saveToLocalStorage()
 
     checkEmptyList();
@@ -138,21 +136,53 @@ function saveToLocalStorage(){
 }
 
 function renderTask(task){
+
     // Разметка задачи
     const taskHTML =`
-        <li id="${task.id}" class="tasks__list-item ${task.done ? 'active' : ''}">
-            <input type="text" class="tasks__list-text" value="${task.title}" readonly></input>
-            <div class="tasks__list-buttons">
-                <input type="checkbox" class="tasks__list-input done-btn" data-action="done" ${task.done ? 'checked' : ''}></input>
-                <button class="tasks__list-btn edit-btn" data-action="edit">
-                    <i class="fa-solid fa-pen"></i>
-                </button>
-                <button class="tasks__list-btn delete-btn" data-action="delete">
-                    <i class="fa-solid fa-trash"></i>
-                </button>
-            </div>
-        </li>
+    <li id="${task.id}" class="tasks__list-item ${task.done ? 'active' : ''}">
+        <input type="text" class="tasks__list-text" value="${task.title}" readonly></input>
+        <div class="tasks__list-buttons">
+            <input type="checkbox" class="tasks__list-input done-btn" data-action="done" ${task.done ? 'checked' : ''}></input>
+            <button class="tasks__list-btn edit-btn" data-action="edit">
+                <i class="fa-solid fa-pen"></i>
+            </button>
+            <button class="tasks__list-btn delete-btn" data-action="delete">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </div>
+    </li>
     `;
-
     tasksList.insertAdjacentHTML('beforeend', taskHTML)
+
 }
+
+function renderTaskFromLocalStorage(taskStatus = 'all'){
+    if(localStorage.getItem('tasks')){
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.forEach(task => {
+            if(taskStatus === task.status || taskStatus === "all"){
+                renderTask(task)
+            }
+        });
+    }
+}
+
+// filtersBtn btns
+const filtersBtn = document.querySelectorAll('.sorting-btn')
+filtersBtn.forEach(item => {
+    item.addEventListener('click', selectSortingBtn)
+})
+function selectSortingBtn(){
+    filtersBtn.forEach(item => {
+        item.classList.remove('active');
+    })
+    this.classList.add('active');
+    dataBtn = this.getAttribute('data-action');
+
+    let oldList = document.querySelectorAll('.tasks__list-item');
+    oldList.forEach(item => item.remove())
+    
+    renderTaskFromLocalStorage(dataBtn);
+}
+
+
